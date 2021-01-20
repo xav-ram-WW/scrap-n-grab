3e1f22d619ce88804d52962883e90de00d5f77a7
OnyxTheGreat <xavierramos1221@gmail.com>
2021-01-19 21:44:57 -0800

    simple scraper with web example; testing git push  and commit from vs2019

---
+++ scraper.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+# creating empty list
+urls = []
+
+# function created
+def scrape(site):
+    # getting the request from url
+    r = requests.get(site)
+    # converting the text
+    s = BeautifulSoup(r.text, "html.parser")
+    for i in s.find_all("a"):
+        href = i.attrs['href']
+        if href.startswith("/"):
+            site = site + href
+            if site not in urls:
+                urls.append(site)
+                print(site)
+                # calling the scrape function itself
+                # generally called recursion
+                scape(site)
+
+# main function
+if __name__ == "__main__":
+    # website to be scrape 
+    site = "http://example.webscraping.com//"
+    # calling function 
+    scrape(site)
